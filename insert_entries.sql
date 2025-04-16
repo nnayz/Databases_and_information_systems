8cb725ee06b6eaf56c11f3afd8a6f3cbcea35f23
nnassrull <nasrul2001@gmail.com>
2025-04-16 15:32:01 +0200

    First DIS Exercise, setup and everything

---
+++ insert_entries.sql
@@ -0,0 +1,6 @@
+INSERT INTO demo (username, password, email) VALUES ('nayz', 'Hello123', 'nayz@gmail.com');
+INSERT INTO demo (username, password, email) VALUES ('john', 'Hello123', 'john@gmail.com');
+INSERT INTO demo (username, password, email) VALUES ('jane', 'Hello123', 'jane@gmail.com');
+INSERT INTO demo (username, password, email) VALUES ('jim', 'Hello123', 'jim@gmail.com');
+INSERT INTO demo (username, password, email) VALUES ('jill', 'Hello123', 'jill@gmail.com');
+INSERT INTO demo (username, password, email) VALUES ('jeff', 'Hello123', 'jeff@gmail.com');
