0ac38f85e92f971b20add1bd909c5e42d7173836
Aldo Sula <aldo-sula@outlook.com>
2025-05-14 01:35:59 +0200

    Added Rollback tests

--- exercise_3/Abort rollback tests/05_deadlock.sql
+++ exercise_3/Abort rollback tests/05_deadlock.sql
@@ -1,10 +1,13 @@
+
+-- In SESSION 1
 \set AUTOCOMMIT off
 BEGIN;
 UPDATE sheet3 SET name = 'X1' WHERE id = 1;
 -- In SESSION 2:
--- BEGIN;
--- UPDATE sheet3 SET name = 'Y1' WHERE id = 2;
--- Then back here:
--- UPDATE sheet3 SET name = 'X2' WHERE id = 2;
--- Then in SESSION 2:
--- UPDATE sheet3 SET name = 'Y2' WHERE id = 1;
+BEGIN;
+UPDATE sheet3 SET name = 'Y1' WHERE id = 2;
+-- In SESSION 1
+UPDATE sheet3 SET name = 'X2' WHERE id = 2;
+
+-- In SESSION 2:
+UPDATE sheet3 SET name = 'Y2' WHERE id = 1;
