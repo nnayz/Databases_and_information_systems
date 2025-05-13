e5a65589031d5028e28e8fddbc313f4044bf3e31
Nasrul Huda <nasrul2001@gmail.com>
2025-05-14 01:32:03 +0200

    RC Scheduling

--- exercise_3/database/db.py
+++ exercise_3/database/db.py
@@ -1,26 +1,42 @@
 import psycopg2
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-
+from sqlalchemy import create_engine
 load_dotenv()
 
-def connect_to_db():
-    conn = psycopg2.connect(
-        host=os.getenv("PGHOST"),
-        database=os.getenv("PGDATABASE"),
-        user=os.getenv("PGUSER"),
-        password=os.getenv("PGPASSWORD")
-    )
+DB_PARAMS = {
+    'host': os.getenv("PGHOST"),
+    'database': os.getenv("PGDATABASE"),
+    'user': os.getenv("PGUSER"),
+    'password': os.getenv("PGPASSWORD"),
+    'port': os.getenv("PGPORT"),
+}
+
+def get_connection(isolation_level=None):
+    """
+    Establishes a database connection.
+    If isolation_level is provided, it will be set for the connection.
+    """
+    conn = psycopg2.connect(**DB_PARAMS)
+    if isolation_level:
+        if isolation_level == "READ COMMITTED":
+            conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_READ_COMMITTED)
+        elif isolation_level == "SERIALIZABLE":
+            conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_SERIALIZABLE)
+        else:
+            raise ValueError(f"Unsupported isolation level: {isolation_level}")
     return conn
 
-def get_session():
-    session = sessionmaker(bind=get_engine())
-    return session()
+def get_cursor(conn):
+    """
+    Returns a cursor for the connection.
+    """
+    return conn.cursor()
 
 def get_engine():
-    # print(os.getenv("DATABASE_URL"))
-    return create_engine(os.getenv("DATABASE_URL"))
+    """
+    Returns an SQLAlchemy engine for the database.
+    """
+    return create_engine(f"postgresql://{DB_PARAMS['user']}:{DB_PARAMS['password']}@{DB_PARAMS['host']}:{DB_PARAMS['port']}/{DB_PARAMS['database']}")
 
-    
+
--- exercise_3/scheduling.py
+++ exercise_3/scheduling.py
@@ -1,45 +1,156 @@
-from database.db import get_session, get_engine
-from sqlalchemy import Column, Integer, String
+from database.db import get_connection, get_cursor, get_engine
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import inspect
-Base = declarative_base()
 
-class ASCII(Base):
-    __tablename__ = "ascii"
-    id = Column(Integer, primary_key=True)
-    letter = Column(String)
+Base = declarative_base()
+engine = get_engine()
+
+class Items(Base):
+    __tablename__ = "items"
+
+    name = Column(String, primary_key=True)
     value = Column(Integer)
 
-engine = get_engine()
-def create_tables():
-    if not inspect(engine).has_table('ascii'):
-        Base.metadata.create_all(bind=engine)
-        populate_items()
-    else:
-        print("Table already exists")
+Base.metadata.create_all(engine) # Create the table
+
+# -- Transactions Operations -- #
+# T1 sets x=100 and y=110
+# T2 sets x=200 and y=220
+
+INITIAL_VALUES = {
+    "x": 0,
+    "y": 0
+}
+
+
+def print_table():
+    with get_connection() as conn:
+        with get_cursor(conn) as cursor:
+            cursor.execute("SELECT * FROM items")
+            for row in cursor:
+                print(row)
+
+
+def schedule_s1():
+
+    
+    
+    # First connection
+    conn1 = get_connection(isolation_level="READ COMMITTED")
+    cursor1 = get_cursor(conn1)
+
+    # Initialize the database using first connection
+    cursor1.execute("DELETE FROM items")
+    cursor1.execute("INSERT INTO items (name, value) VALUES (%s, %s)", ("x", 0))
+    cursor1.execute("INSERT INTO items (name, value) VALUES (%s, %s)", ("y", 0))
+    conn1.commit()
+
+    # Second connection
+    conn2 = get_connection(isolation_level="READ COMMITTED")
+    cursor2 = get_cursor(conn2)
+
+    # r1(x)
+    cursor1.execute("SELECT * FROM items WHERE name = 'x'")
+    x_value = cursor1.fetchone()[1]
+
+    # w2(x)
+    cursor2.execute("UPDATE items SET value = 200 WHERE name = 'x'")
+    
+    # c2
+    conn2.commit()
+
+    # w1(x)
+    cursor1.execute("UPDATE items SET value = 100 WHERE name = 'x'")
+
+    # r1(x)
+    cursor1.execute("SELECT * FROM items WHERE name = 'x'")
+    x_value = cursor1.fetchone()[1]
+
+    # c1
+    conn1.commit()
+    
+def schedule_s2():
+    # initialize_database using first connection
+    conn1 = get_connection(isolation_level="READ COMMITTED")
+    cursor1 = get_cursor(conn1)
+    cursor1.execute("DELETE FROM items")
+    cursor1.execute("INSERT INTO items (name, value) VALUES (%s, %s)", ("x", 0))
+    cursor1.execute("INSERT INTO items (name, value) VALUES (%s, %s)", ("y", 0))
+    conn1.commit()
+
+    # Second connection
+    conn2 = get_connection(isolation_level="READ COMMITTED")
+    cursor2 = get_cursor(conn2)
+
+    # r1(x)
+    cursor1.execute("SELECT * FROM items WHERE name = 'x'")
+    x_value = cursor1.fetchone()[1]
+
+    # w2(x)
+    cursor2.execute("UPDATE items SET value = 200 WHERE name = 'x'")
+    
+    # c2
+    conn2.commit()
+
+    # r1(x)
+    cursor1.execute("SELECT * FROM items WHERE name = 'x'")
+    x_value = cursor1.fetchone()[1]
+
+    # c1
+    conn1.commit()
+
+def schedule_s3():
+    # initialize_database using first connection
+    conn1 = get_connection(isolation_level="READ COMMITTED")
+    cursor1 = get_cursor(conn1)
+    cursor1.execute("DELETE FROM items")
+    cursor1.execute("INSERT INTO items (name, value) VALUES (%s, %s)", ("x", 0))
+    cursor1.execute("INSERT INTO items (name, value) VALUES (%s, %s)", ("y", 0))
+    conn1.commit()
+
+    # Second connection
+    conn2 = get_connection(isolation_level="READ COMMITTED")
+    cursor2 = get_cursor(conn2)
+
+    # r2(x)
+    cursor2.execute("SELECT * FROM items WHERE name = 'x'")
+    x_value = cursor2.fetchone()[1]
+
+    # w1 (x)
+    cursor1.execute("UPDATE items SET value = 100 WHERE name = 'x'")
+
+    # w1(y)
+    cursor1.execute("UPDATE items SET value = 110 WHERE name = 'y'")
+
+    # c1
+    conn1.commit()
+    
+    # r2(y)
+    cursor2.execute("SELECT * FROM items WHERE name = 'y'")
+    y_value = cursor2.fetchone()[1]
+
+    # w2(x)
+    cursor2.execute("UPDATE items SET value = 220 WHERE name = 'x'")
+
+    # w2(y)
+    cursor2.execute("UPDATE items SET value = 220 WHERE name = 'y'")
+
+    # c2
+    conn2.commit()
     
 
-def populate_items():
-    session = get_session()
-    for i in range(65, 90, 1): # 65 is the ASCII code for A, 90 is the ASCII code for Z
-        session.add(ASCII(id=i, letter=chr(i), value=i))
+if __name__ == "__main__":
+    # schedule_s1()
 
-    for i in range(97, 122, 1): # 97 is the ASCII code for a, 122 is the ASCII code for z
-        session.add(ASCII(id=i, letter=chr(i), value=i))
+    # print("--------Result of schedule s1:---------")
+    # print_table()
 
-    session.commit()
+    # schedule_s2()
 
-def get_ascii(letter: str):
-    session = get_session()
-    return session.query(ASCII.value).filter(ASCII.letter == letter).first().value
+    # print("--------Result of schedule s2:---------")
+    # print_table()
 
-create_tables()
+    schedule_s3()
 
-letter = input("Enter a letter: ")
-print(get_ascii(letter))
-
-
-
-
-
-
+    print("--------Result of schedule s3:---------")
+    print_table()
